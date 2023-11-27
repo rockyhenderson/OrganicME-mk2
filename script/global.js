1c5ffc6cbfe33a9200ec04b67f6cfcf786b7ee0c
rockyhenderson <jack@teamhendo.com>
2023-11-27 12:28:21 +0000

    minor fix's for live

--- script/global.js
+++ script/global.js
@@ -8,14 +8,14 @@ function toggleNavbarNavAltMarkup() {
       
     } else {
       navbarNavAltMarkup.classList.add("show");
-      navbar.style.backgroundColor = '#57ac49';
+      navbar.style.backgroundColor = '#445A3E';
     }
   }
   //hamburger fix
   window.addEventListener('scroll', function() {
     var navbar = document.getElementById('navbar');
     if (window.scrollY > 0) {
-      navbar.style.backgroundColor = '#57ac49';
+      navbar.style.backgroundColor = '#445A3E';
       navbar.style.borderBottom = '1px lightgrey solid';
     } else {
       navbar.style.backgroundColor = ''; 
